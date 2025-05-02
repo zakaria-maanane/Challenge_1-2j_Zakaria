f43ccedbcfdce9f02b80c458ffac9457518b1a4f
zakaria-maanane <zakaria.maanane@mail.io>
2025-05-02 23:59:57 +0100

    mappe

---
+++ Programme_challenge_9.py
@@ -0,0 +1,23 @@
+""Challenge 9 carte France """"""
+
+
+france_map = """
+       _______
+     /        \\
+    /  BRETAGNE\\
+   /            \\
+  |   NORMANDIE  |
+  |     ______   | HAUTS-DE-FRANCE
+  |    |PARIS|---|
+  |    |_____|   |
+  |     CENTRE   | GRAND EST
+  |     ______   |
+  |    |LYON |   |
+  |    |_____|   \\____
+  |   AUVERGNE        \\
+  |       \\_________  |
+  \\__________OCCITANIE|
+             \\_______/ MÉDITERRANÉE
+"""
+
+print(france_map)
